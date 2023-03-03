87bc40e38f7b468eaaec0181f7b425131bee1616
alvaro lopes <alvarolopes2020@protonmail.com>
2023-03-03 00:15:22 -0300

    refactor: changed cors endpoint

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(function (req, res, next) {
     // update to match the domain you will make the request from
 
-    res.header("Access-Control-Allow-Origin", "https://zohan-tcc-backend.vercel.app"); 
+    res.header("Access-Control-Allow-Origin", "https://zohan-front-tcc.vercel.app"); 
     //res.header("Access-Control-Allow-Origin", "http://localhost:8080"); 
     
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
